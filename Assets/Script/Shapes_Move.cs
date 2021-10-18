af4349ea112a4d004f961cf665a19ffc554b56da
haddanreda <haddan.reda.net@gmail.com>
2021-10-18 22:59:58 +0100

    all-important-bugs_fixed_need-limits-manipulation_saving-changing-line-mouvement

--- Assets/Script/Shapes_Move.cs
+++ Assets/Script/Shapes_Move.cs
@@ -11,10 +11,19 @@ public class Shapes_Move : MonoBehaviour
 
     private GameObject[,] shapes = new GameObject[2, 3];
 
+    private GameObject shape_aux;
+
+    private int[,] shape_index_aux = new int[3, 2];
+    private int index_save;
+
     public Vector3[] nextposition;
+
+    private Vector3 position_aux;
 
     public int[,] currentindex = new int[,] { { 0, 0 }, { 0, 0 }, { 0, 0 } };
     public int[,] nextindex = new int[,] { { 0, 0 }, { 0, 0 }, { 0, 0 } };
+
+    public int[,] center = new int[,] { };
 
     public int[] currentmove = new int[] { 0, 0 };
     public int[] nextmove = new int[] { 0, 0 };
@@ -112,10 +121,26 @@ public class Shapes_Move : MonoBehaviour
 
                 if (nextindex[i, 1] == 2)
                 {
+                    index_save = i;
                     shapes[Mathf.Abs(nextindex[i, 0] - 1), 1] = shapes[nextindex[i, 0], i];
                 }
             }
-            currentmove = new int[] { 0, 0 };
+
+            //switch the two gameobjects with their positions
+
+            shape_aux = shapes[nextindex[index_save, 0], 1];
+            shapes[nextindex[index_save, 0], 1] = shapes[nextindex[index_save, 0], index_save];
+            shapes[nextindex[index_save, 0], index_save] = shape_aux;
+
+            position_aux = nextposition[1];
+            nextposition[1] = nextposition[index_save];
+            nextposition[index_save] = position_aux;
+
+            currentmove[1] = 0;
+
+            Debug.Log(shapes[0, 0].name + "," + shapes[0, 1].name + "," + shapes[0, 2].name);
+            Debug.Log(shapes[1, 0].name + "," + shapes[1, 1].name + "," + shapes[1, 2].name);
+
 
         }
 
@@ -126,14 +151,14 @@ public class Shapes_Move : MonoBehaviour
             {
                 if (shapes[nextindex[i, 0], i].transform.position != nextposition[i])
                 {
-                    shapes[nextindex[i, 0], i].transform.position = Vector3.MoveTowards(shapes[nextindex[i, 0], i].transform.position, nextposition[i], Time.deltaTime);
+                    shapes[nextindex[i, 0], i].transform.position = Vector3.MoveTowards(shapes[nextindex[i, 0], i].transform.position, nextposition[i], Time.deltaTime * 10);
                     k += 1;
                 }
             }
             if (k == 0)
             {
                 is_moving = false;
-                currentmove = new int[] { 0, 0 };
+                currentmove[1] = 0;
             }
         }
 
